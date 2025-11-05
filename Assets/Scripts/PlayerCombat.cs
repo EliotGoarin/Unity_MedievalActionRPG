4fa8fa7809446335b15a635caf9325a06ad51b05
Eliot Goarin <eliotg.prepa@gmail.com>
2025-11-05 21:16:42 +0100

    ajout attaque goblin et player animation

--- Assets/Scripts/PlayerCombat.cs
+++ Assets/Scripts/PlayerCombat.cs
@@ -5,6 +5,11 @@ public class PlayerCombat : MonoBehaviour
     public Animator anim;
     public float cooldown = 1;
     private float timer;
+    public Transform attackPoint;
+    public float weaponRange = 1f;
+    public LayerMask enemyLayer;
+    public int damage = 1;
+
 
     void Update()
     {
@@ -18,12 +23,34 @@ public class PlayerCombat : MonoBehaviour
         if (timer <= 0)
         {
             anim.SetBool("isAttacking", true);
+
             timer = cooldown;
         }
     }    
 
+    public void DealDamage()
+    {
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, weaponRange, enemyLayer);
+
+        if (hitEnemies.Length >0)
+        {
+            EnnemyHealth ennemyHealth = hitEnemies[0].GetComponent<EnnemyHealth>();
+            if (ennemyHealth != null)
+            {
+                ennemyHealth.ChangeHealth(-damage);
+            }
+        }
+    }
+
     public void EndAttack()
     {
         anim.SetBool("isAttacking", false);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+
+        Gizmos.DrawWireSphere(attackPoint.position, weaponRange);
     }
 }
